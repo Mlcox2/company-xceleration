6fb7f5cd99d6c41830c2c9420ec85caeee845ef3
Matthew Cox <matthew@xcel.team>
2026-02-01 20:24:36 -0800

    Remove redundant Meet Your Scaling Partners section from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Navbar } from '../components/Navbar';
 import { Hero } from '../components/Hero';
 import { PainPoints } from '../components/PainPoints';
 import { Solution } from '../components/Solution';
-import { Team } from '../components/Team';
 import { Testimonials } from '../components/Testimonials';
 import { Footer } from '../components/Footer';
 
@@ -15,7 +14,6 @@ export const Home = () => {
                 <PainPoints />
                 <Solution />
                 <Testimonials />
-                <Team />
             </main>
             <Footer />
         </div>
