d920b5154dedc5917a3352b74e38cdc58d0192da
Mlcox2 <matthew@xcel.team>
2026-09-20 20:00:20 +0000

    Add team section to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,22 @@ import { TrustBar } from '../components/TrustBar';
 import { PainPoints } from '../components/PainPoints';
 import { Solution } from '../components/Solution';
 import { Testimonials } from '../components/Testimonials';
+import { Team } from '../components/Team';
 import { Footer } from '../components/Footer';
 
 export const Home = () => {
-    return (
-        <div className="bg-background min-h-screen text-text-primary selection:bg-primary/30">
-            <Navbar />
-            <main>
-                <Hero />
-                <TrustBar />
-                <Testimonials />
-                <PainPoints />
-                <Solution />
-            </main>
-            <Footer />
-        </div>
-    );
+	return (
+		<div className="bg-background min-h-screen text-text-primary selection:bg-primary/30">
+			<Navbar />
+			<main>
+				<Hero />
+				<TrustBar />
+				<Testimonials />
+				<PainPoints />
+				<Solution />
+				<Team />
+			</main>
+			<Footer />
+		</div>
+	);
 };
